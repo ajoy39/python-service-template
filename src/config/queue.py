c8d6fe5fdd977e931772135bf3e5cee27e736922
Alec Joy <alec@alecjoy.com>
2025-04-07 12:52:32 -0400

    allow empty config values for non-essentials

--- src/config/queue.py
+++ src/config/queue.py
@@ -5,27 +5,29 @@ from pydantic import RedisDsn, computed_field, HttpUrl
 class QueueSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
 
-    QUEUE_BROKER_HOST: str
-    QUEUE_BROKER_PORT: int
-    QUEUE_BACKEND_HOST: str
-    QUEUE_BACKEND_PORT: int
+    QUEUE_BROKER_HOST: str | None = None
+    QUEUE_BROKER_PORT: int | None = None
+    QUEUE_BACKEND_HOST: str | None = None
+    QUEUE_BACKEND_PORT: int | None = None
 
     @computed_field
     @property
-    def BROKER_URL(self) -> RedisDsn:
-        return RedisDsn.build(
-            scheme="redis",
-            host=self.QUEUE_BROKER_HOST,
-            port=self.QUEUE_BROKER_PORT
-        )
+    def BROKER_URL(self) -> RedisDsn | None:
+        if self.QUEUE_BROKER_HOST is not None:
+            return RedisDsn.build(
+                scheme="redis",
+                host=self.QUEUE_BROKER_HOST,
+                port=self.QUEUE_BROKER_PORT
+            )
 
     @computed_field
     @property
-    def BACKEND_URL(self) -> RedisDsn:
-        return RedisDsn.build(
-            scheme="redis",
-            host=self.QUEUE_BACKEND_HOST,
-            port=self.QUEUE_BACKEND_PORT
-        )
+    def BACKEND_URL(self) -> RedisDsn | None:
+        if self.QUEUE_BACKEND_HOST is not None:
+            return RedisDsn.build(
+                scheme="redis",
+                host=self.QUEUE_BACKEND_HOST,
+                port=self.QUEUE_BACKEND_PORT
+            )
 
 queue_settings = QueueSettings()
--- src/config/sentry.py
+++ src/config/sentry.py
@@ -2,7 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import HttpUrl
 
 class SentrySettings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra='ignore',
+        env_ignore_empty=True
+    )
 
     SENTRY_DSN: HttpUrl | None = None
 
